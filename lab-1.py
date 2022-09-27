39e93f8852c09a8275461f287e7759b3c0db20e9
WilliamAnsuok <khafizoff.rodion@gmail.com>
2022-09-27 19:41:34 +0300

    All tasks solved, 27/09/2022

--- lab-1.py
+++ lab-1.py
@@ -3,58 +3,55 @@ import random
 
 flag = 0
 search = input('Search for: ')
-f = open('result.txt', 'w')
 
 count_row = -1                                                   #счётчик строк в таблице
 count_symb = 0                                                   #счётчик книг с названием длинее 30 символов
 
-help_count = 0
+max_pop = 0 #счетчик максимальной популярности
+popularity = [] #массив, в котором содержаться самые популярные книги
+tags = [] #список уникальных тегов
 
-max_pop = 0
-popularity = []
-tags = []
-
-books = []
-authors = []
-years = []
-
-id_s = []
+id_s = [] #айди всех книг
 with open('books.csv', 'r', encoding='cp1251') as csvfile:
     table = csv.reader(csvfile, delimiter=';')
     for row in table:
+
+        count_row += 1              #подсчёт всех записей в таблице исключая шапку
 
         lower_case = row[3].lower()
 
         index = lower_case.find(search.lower())
 
-        if index != -1:
+        if index != -1:             #вывод всех книг по запрошенному автору с учётом условий варианта
             if row[7] != 'Цена поступления':
-                if float(row[7][:-3]) >= 200:
-                    print(row[1])
+                if '.' in row[7]:
+                    if float(row[7][:-3]) >= 200:
+                        print(row[1])
+                else:
+                    if int(row[7]) >= 200:
+                        print(row[1])
             flag = 1
 
-        if len(row[1]) > 30:
+        if len(row[1]) > 30:         #подсчёт кол-ва записей с длинной названия более 30 символов
             count_symb += 1
 
-        count_row += 1
-
-        if row[8] != 'Кол-во выдач':
+        if row[8] != 'Кол-во выдач':    #определение наибольшего значения выдачи книг
             if int(row[8]) > max_pop:
                 max_pop = int(row[8])
 
-        check = list(row[12].split('#')[1:])
+        check = list(row[12].split('#')[1:]) #список тегов для каждой книги
 
-        for i in check:
+        for i in check:                      #список уникальных тегов для всех книг
             if i not in tags:
                 tags.append(i)
 
-        id_s.append(row[0])
+        id_s.append(row[0])                  #добавление айди книги в общий список
 
     if flag == 0:
         print('Not found')
-    f.close()
 
-with open('books.csv', 'r', encoding='cp1251') as csvfile:
+
+with open('books.csv', 'r', encoding='cp1251') as csvfile:  #поиск 20 самых популярных книг
     table = csv.reader(csvfile, delimiter=';')
     if len(popularity) < 20:
         while max_pop >= 0:
@@ -63,24 +60,21 @@ with open('books.csv', 'r', encoding='cp1251') as csvfile:
                     popularity.append(row[1])
             max_pop -= 1
 
-require = []
+require = []                #формирование запроса на 20 рандомных айди книг
 while len(require) != 20:
     ran = random.randrange(0, count_row)
     if id_s[ran] not in require:
         require.append(id_s[ran])
+require.sort()              #сортировка айди по возрастанию
+result = []                 #лист с подготовленными данными для файла result.txt
 
-with open('books.csv', 'r', encoding='cp1251') as csvfile:
+with open('books.csv', 'r', encoding='cp1251') as csvfile:      #формирование списка из 20 рандомных книг, подготовка к записи их в резалт тхт
     table = csv.reader(csvfile, delimiter=';')
     for row in table:
         for i in require:
             if row[0] == i:
-                axil = [row[3], row[1], int(row[6][6:11])]
-                books.append(axil)
-                authors.append(row[3])
-                years.append(int(row[6][6:11]))
-
-authors.sort()
-#years.sort()
+                temp = [row[3], row[1], row[6][6:11]]
+                result.append(temp)
 
 print('Amount of rows in full table:', count_row)
 print('Amount of books with name longer that 30 symbols:', count_symb)
@@ -88,6 +82,9 @@ print('All tags: ', *tags)
 print('The list of most popular books: ')
 for i in range(20):
     print(popularity[i])
-print(books)
-print(authors)
-print(years)
+
+f = open('result.txt', 'w')         #запись 20 рандомных книг в фаул резалт тхт
+for i in range(20):
+    ax = str(i + 1) + '. ' + result[i][0] + '. ' + result[i][1] + ' - ' + result[i][2] + '\n'
+    f.write(ax)
+f.close()
